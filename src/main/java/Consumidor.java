0ba9b6851a192cc6e7974a0657dd6289c68ae042
adrianaalbuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2022-10-10 21:22:45 -0300

    commit

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -3,28 +3,45 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
-
 public class Consumidor {
-    public static void main(String[] args) throws Exception {
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost("localhost");
-        Connection conexao = connectionFactory.newConnection();
-        Channel canal = conexao.createChannel();
 
-        String NOME_FILA = "plica"
-                + "";
-        canal.queueDeclare(NOME_FILA, false, false, false, null);
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
-        DeliverCallback callback = (consumerTag, delivery) -> {
-            String mensagem = new String(delivery.getBody());
-            System.out.println("Eu " + consumerTag + " Recebi: " + mensagem);
+    public static void main (String [] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("mqadmin");
+        factory.setPassword("Admin123XX_");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println ("[*] Aguardando mensagens. Para sair, pressione CTRL + C");
+
+        channel.basicQos(1);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String mensagem = new String (delivery.getBody (), "UTF-8");
+
+            System.out.println ("[x] Recebido '" + mensagem + "'");
+            try {
+                doWork (mensagem);
+            } finally {
+                System.out.println ("[x] Done");
+                channel.basicAck (delivery.getEnvelope (). getDeliveryTag (), false);
+            }
         };
+        channel.basicConsume (TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> {});
+    }
 
-        // fila, noAck, callback, callback em caso de cancelamento (por exemplo, a fila foi deletada)
-        canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {
-            System.out.println("Cancelaram a fila: " + NOME_FILA);
-        });
+    private static void doWork(String task) {
+        for (char ch: task.toCharArray ()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep (1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
-
-
